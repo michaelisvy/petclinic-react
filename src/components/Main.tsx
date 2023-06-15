ddee5e6279456f9202e5686430d37805b0d4f2a6
Michael Isvy <michael.isvy@gmail.com>
2023-06-15 21:37:31 +0800

    formatting

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Pets from './Pets'
-import PetTypes from './PetTypes'
+import Pets from './Pets';
+import PetTypes from './PetTypes';
 import Owners from './Owners';
 
 function Home() {
@@ -16,23 +16,23 @@ function Main() {
   return (
     <Router>
       <nav style={{ margin: 10 }}>
-          <Link to="/" style={{ padding: 5 }}>
+        <Link to="/" style={{ padding: 5 }}>
           Home
-          </Link>
-          <Link to="/pets" style={{ padding: 5 }}>
+        </Link>
+        <Link to="/pets" style={{ padding: 5 }}>
           Pets
-          </Link>
-          <Link to='/owners' style={{ padding: 5 }}>
-            Owners
-          </Link>
-          <Link to="/petTypes" style={{ padding: 5 }}>
+        </Link>
+        <Link to="/owners" style={{ padding: 5 }}>
+          Owners
+        </Link>
+        <Link to="/petTypes" style={{ padding: 5 }}>
           Pet Types
-          </Link>
+        </Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/pets" element={<Pets />} />
-        <Route path='/owners' element={<Owners />} />
+        <Route path="/owners" element={<Owners />} />
         <Route path="/petTypes" element={<PetTypes />} />
       </Routes>
     </Router>
