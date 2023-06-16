280ece44181aee5de5fa7c219ff64d398974bf1b
Michael Isvy <michael.isvy@gmail.com>
2023-06-16 20:19:49 +0800

    moved to axios

--- src/service/PetService.ts
+++ src/service/PetService.ts
@@ -4,16 +4,16 @@ class PetService {
   private url: string = 'http://localhost:9966/petclinic/api';
 
   findAllPets = (): Promise<any> => {
-    return fetch(this.url + '/pets')
-      .then((response) => response.json())
+    return axios.get(this.url + '/pets')
+      .then((response) => response.data)
       .catch((error) => {
         console.error(error);
       });
   };
 
   findAllOwners = (): Promise<any> => {
-    return fetch(this.url + '/owners')
-      .then((response) => response.json())
+    return axios.get(this.url + '/owners')
+      .then((response) => response.data)
       .catch((error) => {
         console.error(error);
       });
